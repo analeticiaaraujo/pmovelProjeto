99f1bf71bd1fbc3de4dd9d10135df76d99f38e70
analeticiaaraujo <alga1@aluno.ifal.edu.br>
2024-03-18 09:26:42 -0300

    texto da idade ajustado do singular para o plural

--- Gato.js
+++ Gato.js
@@ -1,7 +1,7 @@
 import { View, Text } from "react-native";
 
 const Gato = ( {nome, idade} ) => {
-    const texto = idade > 1 ? "anos" : "anos";
+    const texto = idade > 1 ? "anos" : "ano";
 
     return(
         <View>
